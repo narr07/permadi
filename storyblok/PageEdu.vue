669afe96f2a59582c11fa057b9817c14f7dd0a57
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-12-01 11:12:13 +0700

    Add @tailwindcss/typography dependency and update package.json

--- storyblok/PageEdu.vue
+++ storyblok/PageEdu.vue
@@ -3,19 +3,17 @@
   const props = defineProps({ blok: Object });
 </script>
 <template>
-  <UContainer v-editable="blok" class="max-w-3xl mx-auto mt-16 mb-20">
-    <div class="mb-6 w-full mx-auto text-center">
-      <h2 class="text-g2 md:text-g3">{{ blok.headline}}</h2>
-    </div>
-    <div>
-      <template>
-        <UAccordion :items="blok.list">
-          <template #item="{ item }">
-            <UBadge :label="item.years"  class="mb-2" />
-            <p>{{ item.content }}</p>
-          </template>
-        </UAccordion>
-      </template>
-    </div>
-  </UContainer>
+  <div>
+    <UContainer v-editable="blok" class="max-w-3xl mx-auto mt-16 mb-20">
+      <div class="mb-6 w-full mx-auto text-center">
+        <h2 class="text-g2 md:text-g3">{{ blok.headline }}</h2>
+      </div>
+      <UAccordion :items="blok.list">
+        <template #item="{ item }">
+          <UBadge :label="`Tahun: ${item.years}`" class="mb-2" />
+          <p>{{ item.content }}</p>
+        </template>
+      </UAccordion>
+    </UContainer>
+  </div>
 </template>
--- storyblok/PageHero.vue
+++ storyblok/PageHero.vue
@@ -1,5 +1,5 @@
 <script setup>
-  const props = defineProps({ blok: Object });
+const props = defineProps({ blok: Object });
 </script>
 <template>
   <UContainer v-editable="blok">
@@ -8,15 +8,11 @@
         <h1 class="text-g3 lg:text-g4 text-center" itemprop="headline">
           {{ blok.headline }}
         </h1>
-        <div class="mt-5 max-w-3xl text-center mx-auto">
-          <p itemprop="description">
-            {{ blok.subheadline }}
-          </p>
-        </div>
+        <p class="mt-5 max-w-3xl text-center mx-auto text-body-dark" itemprop="description">
+          {{ blok.subheadline }}
+        </p>
         <div class="mt-8 gap-3 flex justify-center">
-          <UButton
-            :to="blok.cta_url"
-            class="animate-bounce ring-1 ring-primary-800">
+          <UButton :to="blok.cta_url" class="animate-bounce ring-1 ring-primary-800">
             <Icon :name="blok.cta_icon.icon" />
             {{ blok.cta }}
           </UButton>
@@ -24,70 +20,4 @@
       </div>
     </header>
   </UContainer>
-  <UContainer class="pb-20">
-    <!-- Grid -->
-    <div class="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
-      <UCard class="relative drop-shadow-2 dark:drop-shadow-2gelap">
-        <Swiper
-          :slides-per-view="1"
-          :modules="[SwiperAutoplay, SwiperPagination]"
-          :loop="true"
-          :pagination="true"
-          :autoplay="{
-            delay: 5000,
-            disableOnInteraction: true,
-          }">
-          <SwiperSlide>
-            <div class="flex items-center justify-center">
-              <svgGuru />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div class="flex items-center justify-center">
-              <SvgProgrammer />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div class="flex items-center justify-center">
-              <SvgDesigner />
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </UCard>
-      <div>
-        <h1 class="block text-g3">
-          {{ blok.headline }}
-        </h1>
-        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">
-          {{ blok.subheadline }}
-        </p>
-        <!-- Buttons -->
-        <div class="mt-7 grid gap-3 w-full sm:inline-flex">
-          <a
-            class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-            href="#">
-            Get started
-            <svg
-              class="flex-shrink-0 w-4 h-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round">
-              <path d="m9 18 6-6-6-6" />
-            </svg>
-          </a>
-        </div>
-        <!-- End Buttons -->
-        <!-- Review -->
-      </div>
-      <!-- End Col -->
-    </div>
-    <!-- End Grid -->
-  </UContainer>
-  <!-- End Hero -->
 </template>
