bd2914ec60f071ba75256c80111a4086d109f1f4
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-12-04 13:00:12 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -80,21 +80,21 @@ export default defineNuxtConfig({
     description: "Personal website of Dinar Permadi Yusup",
     defaultLocale: "id",
   },
-  nitro: {
-    prerender: {
-      crawlLinks: true,
-    },
-  },
-  routeRules: {
-    "/":{ swr: 60 * 10},
-    "/gallery":{ swr: 60 * 10},
-    "/blog/**": {
-      swr: 60 * 10,
-      prerender: true,
-      // isr: 60,
-      // isr: true,
-    },
-  },
+  // nitro: {
+  //   prerender: {
+  //     crawlLinks: true,
+  //   },
+  // },
+  // routeRules: {
+  //   "/":{ swr: 60 * 10},
+  //   "/gallery":{ swr: 60 * 10},
+  //   "/blog/**": {
+  //     swr: 60 * 10,
+  //     prerender: true,
+  //     // isr: 60,
+  //     // isr: true,
+  //   },
+  // },
   // runtimeConfig: {
   //   public: {
   //     NODE_ENV: process.env.NODE_ENV
