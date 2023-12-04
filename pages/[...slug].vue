28f60ea9f40e3c2f0d49b6194d47c4e38b057f42
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-12-04 14:39:07 +0700

    Remove unused code and comments

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,36 +1,19 @@
 <script setup>
-  const { slug } = useRoute().params
-const url = slug && slug.length > 0 ? slug.join('/') : 'home'
-const isPreview = useRuntimeConfig().public.NODE_ENV !== 'production'
-const { locale } = useI18n()
-const resolveRelations = ['popular-articles.articles']
-const { data: story, pending } = await useAsyncData(
-  `${locale.value}-${url}`,
-  async () => {
-    const { data } = await useStoryblokApi().get(`cdn/stories/${url.replace(/\/$/, '')}`, {
-      version: isPreview ? 'draft' : 'published',
+  const { slug } = useRoute().params;
+  const { locale } = useI18n();
+  const resolveRelations = ["popular-articles.articles"];
+  const url = slug && slug.length > 0 ? slug.join("/") : "home";
+  const story = await useAsyncStoryblok(
+    url.replace(/\/$/, ""),
+    {
+      version: "draft",
       language: locale.value,
-      resolve_relations: resolveRelations
-    })
-    return data?.story
-  },
-)
-if (!isPreview) {
-  if (!story.value) {
-    showError({ statusCode: 404, statusMessage: "Page Not Found" })
-  }
-}
-onMounted(() => {
-  if (isPreview && story.value && story.value.id) {
-    useStoryblokBridge(
-      story.value.id,
-      (evStory) => story.value = evStory,
-      {
-        resolveRelations,
-      }
-    )
-  }
-})
+      resolve_relations: resolveRelations,
+    },
+    {
+      resolveRelations,
+    }
+  );
   //seo
   const metadata = story.value.content.metadata;
   const defaultTitle = "Dinar";
@@ -57,5 +40,5 @@ onMounted(() => {
   });
 </script>
 <template>
- <StoryblokComponent v-if="pending === false && story" :blok="story.content" />
+  <StoryblokComponent v-if="story" :blok="story.content" />
 </template>
