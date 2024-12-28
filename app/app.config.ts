7c58738b71c7d10671bab532445924e9eb580179
narr07 <dinarpermadi07@gmail.com>
2024-12-28 21:18:34 +0700

    feat: add social media configuration to app settings and define schema for navbar icons

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,4 +1,9 @@
 export default defineAppConfig({
+  socials: {
+    twitter: '',
+    github: '',
+    mastodon: '',
+  },
   // https://ui3.nuxt.dev/getting-started/theme#design-system
   ui: {
     colors: {
