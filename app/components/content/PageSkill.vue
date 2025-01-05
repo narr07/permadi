0d82e6c43b766305b6e4fed1f6268d6829e47a01
narr07 <dinarpermadi07@gmail.com>
2025-01-05 15:24:50 +0700

    feat: remove skills collection from PageSkill component and update package.json to remove i18n dependency

--- app/components/content/PageSkill.vue
+++ app/components/content/PageSkill.vue
@@ -1,13 +1,4 @@
 <script lang="ts" setup>
-interface SkillItem {
-  name: string
-  url: string
-  title: string
-}
-
-const skill = await queryCollection('skills')
-  .all() as unknown as { meta: { body: SkillItem[] } }[]
-
 const designIcon = ref([
   {
     title: 'Illustrator',
@@ -217,17 +208,7 @@ const tabitems = [
   <UContainer class="grid grid-cols-1 md:grid-cols-2 gap-4">
     <UCard>
       <div class="grid grid-cols-4 md:grid-cols-6 gap-4">
-        <div v-for="(item, index) in skill?.[0]?.meta?.body" :key="index" class="flex justify-center items-center">
-          <UTooltip :text="item.title">
-            <UButton
-              :to="item.url" color="neutral" variant="soft" square
-              class="hover:scale-110 justify-center transform transition"
-            >
-              <UIcon :name="item.name" class="size-10" />
-              <span class="sr-only">{{ item.name }}</span>
-            </UButton>
-          </UTooltip>
-        </div>
+        Texs
       </div>
     </UCard>
     <UCard>
