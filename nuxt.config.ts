532795e21056d2d2093b1946c2cf416955e04b97
narr07 <dinarpermadi07@gmail.com>
2025-01-27 23:18:14 +0700

    refactor: comment out booster configuration in nuxt.config.ts for improved clarity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -180,28 +180,28 @@ export default defineNuxtConfig({
       '2xl': 1536,
     },
   },
-  booster: {
-    detection: {
-      performance: true,
-      browserSupport: true,
-      battery: true,
-    },
-    performanceMetrics: {
-      timing: {
-        fcp: 800,
-        dcl: 1200,
-      },
-    },
-    optimizeSSR: {
-      cleanPreloads: true,
-      cleanPrefetches: true,
-      inlineStyles: true,
-    },
-    lazyOffset: {
-      component: '0%',
-      asset: '0%',
-    },
-  },
+  // booster: {
+  //   detection: {
+  //     performance: true,
+  //     browserSupport: true,
+  //     battery: true,
+  //   },
+  //   performanceMetrics: {
+  //     timing: {
+  //       fcp: 800,
+  //       dcl: 1200,
+  //     },
+  //   },
+  //   optimizeSSR: {
+  //     cleanPreloads: true,
+  //     cleanPrefetches: true,
+  //     inlineStyles: true,
+  //   },
+  //   lazyOffset: {
+  //     component: '0%',
+  //     asset: '0%',
+  //   },
+  // },
   // vitalizer: {
   //   disablePrefetchLinks: true,
   //   disablePreloadLinks: true,
