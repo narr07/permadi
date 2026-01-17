eb17c43ac3ee1c318220a04446a5c29331c0e7ed
narr07 <dinarpermadi07@gmail.com>
2026-01-18 02:32:06 +0700

    feat: Dynamically load navigation items, update header styling, and add custom prose link styles.

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,6 +3,14 @@ export default defineAppConfig({
     colors: {
       primary: 'brand',
       neutral: 'gray'
+    },
+    prose: {
+      a: {
+        base: [
+          'text-blue-500 border-b border-dashed  border-blue-500 hover:border-blue-600 hover:text-blue-600 font-medium focus-visible:outline-primary focus-visible:has-[>code]:outline-0 [&>code]:border-dashed hover:[&>code]:border-primary hover:[&>code]:text-primary focus-visible:[&>code]:border-primary focus-visible:[&>code]:text-primary',
+          'transition-colors [&>code]:transition-colors'
+        ]
+      }
     }
   }
 })
--- app/app.vue
+++ app/app.vue
@@ -1,44 +1,48 @@
 <script setup lang="ts">
 import type { NavigationMenuItem } from '@nuxt/ui'
 
+const { data: nav } = await useAsyncData('navigation', () => {
+  return queryCollectionNavigation('content')
+})
+
 const route = useRoute()
 
-const items = computed<NavigationMenuItem[]>(() => [{
-  label: 'Home',
-  to: '/',
-  active: route.path.startsWith('/')
-}, {
-  label: 'About',
-  to: '/about',
-  active: route.path.startsWith('/about')
-}])
+const items = computed<NavigationMenuItem[]>(() => {
+  return (nav.value || []).map(item => ({
+    label: item.title,
+    to: item.path,
+    active: item.path === '/' ? route.path === '/' : route.path.startsWith(item.path)
+  }))
+})
 </script>
 
 <template>
   <UApp>
-    <UHeader>
-      <template #title>
-        Tes
-      </template>
+    <UContainer class="fixed top-2 inset-x-0 z-50">
+      <UHeader class="border dark:border-brand-700 border-brand-900 rounded-lg bg-(--ui-bg)/60 backdrop-blur-sm px-4">
+        <template #title>
+          <div class="flex items-center gap-2 text-brand-500 font-bold uppercase ">
+            <UIcon name="i-lucide-compass" class="size-5" />
+            <span>Architect.sys</span>
+          </div>
+        </template>
 
-      <UNavigationMenu :items="items" />
+        <UNavigationMenu :items="items" class="justify-center uppercase   text-xs font-medium" />
 
-      <template #right>
-        <UColorModeButton />
+        <template #right>
+          <UColorModeButton />
+        </template>
 
-      </template>
-
-      <template #body>
-        <UNavigationMenu :items="items" orientation="vertical" class="-mx-2.5" />
-      </template>
-    </UHeader>
+        <template #body>
+          <UNavigationMenu :items="items" orientation="vertical" class="-mx-2.5 uppercase" />
+        </template>
+      </UHeader>
+    </UContainer>
 
     <UMain>
-      <UContainer>
+      <UContainer class="py-6 pt-24">
         <NuxtPage />
       </UContainer>
     </UMain>
-
-    <!-- <UFooter /> -->
   </UApp>
 </template>
