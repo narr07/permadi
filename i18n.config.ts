a1a295cb378cb3cb6563bf30608d7b25a202dfbe
Dinar, Permadi <dinarpermadi07@gmail.com>
2026-07-23 13:25:37 +0700

    chore: clear all files to start fresh from scratch

--- i18n.config.ts
+++
@@ -1,8 +0,0 @@
-import { fallbackLocales } from './i18n-constants'
-
-// i18n configuration for @nuxtjs/i18n
-// This file is consumed by vueI18n option in nuxt.config.ts
-export default {
-  fallbackLocale: fallbackLocales,
-  legacy: false, // Use Composition API mode
-}
--- server/db/migrations/sqlite/0001_eager_bloodstorm.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `gallery_likes` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`gallery_stem` text NOT NULL,
-	`ip_address` text NOT NULL,
-	`created_at` integer NOT NULL
-);
