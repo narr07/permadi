9e22b08accc0aa5386a2a59f58f6401c8e4be989
Dinar, Permadi <dinarpermadi07@gmail.com>
2026-08-13 01:57:23 +0700

    chore: remove better-sqlite3 dependency and configure native sqlite connector for nuxt content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
   ],
+  content: {
+    experimental: {
+      sqliteConnector: 'native'
+    }
+  },
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
 })
