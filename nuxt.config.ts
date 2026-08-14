670e48bc4d5fb11ec401ad719c691b75ed05efed
Dinar, Permadi <dinarpermadi07@gmail.com>
2026-08-15 00:02:55 +0700

    feat: add better-sqlite3 dependency and enable node compatibility for Cloudflare in Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-  ],
-  content: {
-     database: {
-      type: 'd1',
-      bindingName: 'DB'  // Sesuaikan dengan 'Name' di Cloudflare Dashboard Anda
-    },
-  },
-  nitro: {
-    preset: 'cloudflare_pages', // Pastikan menggunakan preset yang sesuai
-    prerender:{
-      routes: ['/'], // Tambahkan rute yang ingin Anda prerender
-      crawlLinks: true
-    }
-  },
-  devtools: { enabled: true },
-  compatibilityDate: '2024-04-03',
+	modules: [
+		'@nuxt/content',
+	],
+	content: {
+		database: {
+			type: 'd1',
+			bindingName: 'DB',
+		},
+	},
+	nitro: {
+		preset: 'cloudflare_pages',
+		prerender: {
+			routes: ['/'],
+			crawlLinks: true,
+		},
+		cloudflare: {
+			nodeCompat: true,
+		},
+	},
+	devtools: { enabled: true },
+	compatibilityDate: '2024-04-03',
 })
